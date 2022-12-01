22702506df799b6d20e2ad02801335383cedeb3c
csanmont <csanmont@emeal.nttdata.com>
2022-11-30 23:12:46 -0500

    version inicial

---
+++ src/main/java/com/nttdata/microservicebankgateway/MicroserviceBankGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.microservicebankgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceBankGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceBankGatewayApplication.class, args);
+	}
+
+}
